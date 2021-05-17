46d2e1c3c2d96af600613f81845fb49af71d2c4a
xiaolongz007 <84307021+xiaolongz007@users.noreply.github.com>
2021-05-17 18:58:41 +0800

    feat: flexible Toc structure (#170)
    * flexible Toc structure fixed -- filenames need to be prefixed with order 1-, 2- etc
    
    * nested directory(ignored) and string name sorting issue fixed

--- docs/.vuepress/sidebar/guide.js
+++ docs/.vuepress/sidebar/guide.js
@@ -1,9 +1,30 @@
-module.exports.JSGuide = function (groupA) {
+"use strict";
+const fs = require("fs");
+
+function toToc(dir) {
+  let fileNames = fs.readdirSync(dir);
+  let names = [];
+  for (let i in fileNames) {
+    let name = fileNames[i];
+    let path = dir + "/" + name;
+    if (fs.statSync(path).isDirectory()) {
+      continue;
+    }
+    if (name !== "README.md"){
+      names.push(fileNames[i].split(".")[0]);
+    }
+  }
+  names.unshift("");
+  return names;
+}
+
+module.exports.JSGuide = function (groupA, dirName) {
+  let names = toToc(dirName);
   return [
     {
       title: groupA,
       collapsable: false,
-      children: ["", "getting-started", "basic-config"],
+      children: names,
     },
   ];
 };
